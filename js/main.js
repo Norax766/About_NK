efe7ab10a87795aa11b2cc344e5332e0e090d5ba
Karu <109097962+Norax766@users.noreply.github.com>
2026-05-25 15:59:15 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -451,34 +451,12 @@ window.addEventListener("load", () => {
 const discordModal =
   document.getElementById("discord-modal");
 
-const discordLoading =
-  document.querySelector(".discord-loading");
-
-const discordContent =
-  document.querySelector(".discord-content");
-
-
-// 🔥 abrir modal
 function openDiscordModal(){
 
   discordModal.classList.add("active");
 
-  discordLoading.style.display = "flex";
-  discordContent.style.display = "none";
-
-  // fake loading aesthetic
-  setTimeout(()=>{
-
-    discordLoading.style.display = "none";
-
-    discordContent.style.display = "block";
-
-  }, 1800);
-
 }
 
-
-// 🔥 cerrar click fuera
 discordModal.addEventListener("click",(e)=>{
 
   if(e.target.id === "discord-modal"){
